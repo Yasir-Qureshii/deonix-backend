63bbb20831b3110267dc9460a5d45d572f05c9fe
Yasir Qureshi <yasirqr25@gmail.com>
2023-08-21 18:02:07 +0500

    changed allow-origin to 5173

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const products = require("./utils/products");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
